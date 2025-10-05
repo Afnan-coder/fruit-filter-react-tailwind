a61858a402205806265dd11a1bc5a63397423b26
Afnan-coder <muhammadafnanali2@gmail.com>
2025-10-05 16:04:11 +0500

    Build the UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ import './App.css'
 function App() {
   return (
     <>
-   
+      <div className="container w-1/2 h-1/2 bg-green-700 mx-auto my-5 rounded-sm boxShadow p-10 text-center">
+        <h1 className='text-4xl my-9 font-bold text-white'>Fruit  Filter</h1>
+        <input type="text" className='w-full outline-green-400 rounded-sm p-3 text-2xl bg-white mb-9' placeholder='Enter fruit' onChange={(e) => setFruit(e.target.value)} />
+        <div className="content h-full max-h-72 overflow-y-scroll">
+          <ul className='overflow-y-auto '>
+            <li>Apple</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
